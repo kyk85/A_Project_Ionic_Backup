2a4bc8965c1fd871933447c165436a63859bd15f
YK <kay2kay1@gmail.com>
2018-03-27 22:01:22 +0800

    2nd Commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+// Custom Imports
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  enquiryForm: FormGroup;
+
+  constructor(public navCtrl: NavController,
+              public formBuilder: FormBuilder) {
+
+    this.enquiryForm = formBuilder.group ({
+      name:[''],
+      email:[''],
+      comments:['']
+    })
 
   }
 
